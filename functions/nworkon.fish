ed7c66c10af434cf986b7ac12fe92d5421aa5dd2
Liam Dawson <liam@ldaws.com>
2019-09-12 09:36:51 +1000

    Add missing __fw_projects for workon completions

--- functions/nworkon.fish
+++ functions/nworkon.fish
@@ -1,3 +1,7 @@
+function __fw_projects
+  fw -q ls
+end
+
 function nworkon
   set -l script (fw -q gen-workon -x $argv)
 
--- functions/workon.fish
+++ functions/workon.fish
@@ -1,3 +1,7 @@
+function __fw_projects
+  fw -q ls
+end
+
 function workon
   set -l script (fw -q gen-workon $argv)
 
